31990a7d33bb12f501768129b18c4c83eb9b4e2d
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-07-30 16:48:19 +0200

    reorganize. combine all scripts in one file. publish the new version as pypi

--- example.py
+++ example.py
@@ -1,15 +1,3 @@
-# SymDFA2Aiger
-The logic library is currently compiled locally and can be found under the folder dist. 
-Use pip install to install pylogics_modalities with all the relevant classes. 
-
-pip install dependencies/pylogics_modalities-0.2.2-py3-none-any.whl
-
-
-pip install multipledispatch
-from multipledispatch import dispatch
-
-
-
 
 """Modify the formula with base operators visitor."""
 # from help import TreeNode
